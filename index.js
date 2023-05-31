61047532ce17612a805a9d4a96dff57caf967517
ShwetaMishra0101 <93873372+ShwetaMishra0101@users.noreply.github.com>
2023-05-31 14:16:17 +0530

    get 250 movies data using puppeteer

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const puppeteer = require('puppeteer');
+
+const app = express();
+
+app.get('/top-movies', async (req, res) => {
+  try {
+    const movies = await scrapeMovies();
+    res.json({ movies });
+  } catch (error) {
+    res.status(500).json({ error: 'An error occurred .' });
+  }
+});
+
+async function scrapeMovies() {
+  const browser = await puppeteer.launch({headless:false});
+  const page = await browser.newPage();
+
+  await page.goto('https://www.imdb.com/chart/top/');
+
+  const movieTitles = await page.evaluate(() => {
+    const movieElements = Array.from(document.querySelectorAll('.lister-list tr'));
+    return movieElements.map(movieElement => {
+      const title = movieElement.querySelector('.titleColumn a');
+      const year = movieElement.querySelector('.secondaryInfo');
+      return `${title.textContent.trim()} (${year.textContent.trim()})`;
+    });
+  });
+
+
+  await browser.close();
+
+  return movieTitles;
+}
+
+app.listen(7000, () => {
+  console.log('Server running on localhost: 7000');
+});
